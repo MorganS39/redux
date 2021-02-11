e6a1a85b9bbccd9db49fa2711a0a7bbe124b8df5
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-11 09:32:51 -1000

    Get state from the Redux store

--- reduxIntro.js
+++ reduxIntro.js
@@ -6,3 +6,9 @@ const reducer = (state = 5) => {
 const store = Redux.createStore(reducer);
 
 // Get state from the Redux store
+const store = Redux.createStore(
+  (state = 5) => state
+);
+const currentState = store.getState();
+
+// Define a Redux action
