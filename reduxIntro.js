9880354ae06df29bb7b1af0be708ac8c9e3c1f06
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-11 11:33:36 -1000

    remove an item from an array

--- reduxIntro.js
+++ reduxIntro.js
@@ -347,3 +347,25 @@ const addToDo = (todo) => {
 const store = Redux.createStore(immutableReducer);
 
 // Remove an item from an array
+const immutableReducer = (state = [0,1,2,3,4,5], action) => {
+  switch(action.type) {
+    case 'REMOVE_ITEM':
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1, state.length)
+      ];
+    default:
+      return state;
+  }
+};
+
+const removeItem = (index) => {
+  return {
+    type: 'REMOVE_ITEM',
+    index
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
+
+// Copy an object with Object.assign
