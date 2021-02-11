b0869c6c3a132abf22d8557106c32e73bd649eec
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-11 11:18:09 -1000

    Write a counter with Redux challenge

--- reduxIntro.js
+++ reduxIntro.js
@@ -269,4 +269,33 @@ const store = Redux.createStore(
   Redux.applyMiddleware(ReduxThunk.default)
 );
 
-// Write a counter with Redux
+// Write a counter with Redux (Written from scratch using previous notes)
+const INCREMENT = 'INCREMENT'; // Define a constant for increment action types
+const DECREMENT = 'DECREMENT'; // Define a constant for decrement action types
+
+const counterReducer = (state = 0, action) => {
+    switch(action.type) {
+        case INCREMENT:
+            return state + 1;
+        case DECREMENT:
+            return state - 1;
+        default:
+            return state;
+    }
+}; // Define the counter reducer which will increment or decrement the state based on the action it receives
+
+const incAction = () => {
+    return {
+        type: INCREMENT
+    };
+}; // Define an action creator for incrementing
+
+const decAction = () => {
+    return {
+        type: DECREMENT
+    };
+}; // Define an action creator for decrementing
+
+const store = Redux.createStore(counterReducer); // Define the Redux store here, passing in your reducers
+
+// Never mutate state
