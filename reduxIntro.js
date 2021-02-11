7e54feb382763eb9b11db5416ddd9dd8a546e227
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-11 11:39:38 -1000

    Copy an object with Object.assign

--- reduxIntro.js
+++ reduxIntro.js
@@ -369,3 +369,26 @@ const removeItem = (index) => {
 const store = Redux.createStore(immutableReducer);
 
 // Copy an object with Object.assign
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      return Object.assign({}, state, { status: 'online' });
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
