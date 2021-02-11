aafe7c8d4de0a1d18d7407799ed28dc3a65267b6
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-11 10:22:04 -1000

    Register a store listener

--- reduxIntro.js
+++ reduxIntro.js
@@ -133,3 +133,29 @@ const logoutUser = () => {
 };
 
 // Register a store listener
+const ADD = 'ADD';
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// Global count variable:
+let count = 0;
+
+store.subscribe( () => { ++count } );
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+
+// Combine multiple reducers
