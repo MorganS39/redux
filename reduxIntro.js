ede4b42fa10a0028181ddfb2cbe620dfaa42fd45
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-11 10:01:18 -1000

    use a switch statement to handle multiple actions

--- reduxIntro.js
+++ reduxIntro.js
@@ -58,3 +58,37 @@ const loginAction = () => {
 };
 
 // Use a switch statement to handle multiple actions
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'LOGIN':
+      return {
+        authenticated: true
+      };
+    case 'LOGOUT':
+    return {
+      authenticated: false
+    };
+    default:
+    return defaultState;
+  } 
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
+
+// Use const for action types
